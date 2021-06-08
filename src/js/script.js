55d69c6a5f6c5388d67c1dd6b11d38db788c6628
rwcalinin <rwcalinin@gmail.com>
2021-06-08 11:27:58 +0300

    slider ready

--- src/js/script.js
+++ src/js/script.js
@@ -4,6 +4,8 @@ $(function() {
         dots: true,
         slidesToShow: 1,
         slidesToScroll: 1,
+        nextArrow: '<button type="button" class="slick-arrow slick-next"><img src="images/next-arrow.svg" alt="next slide"></button>',
+        prevArrow: '<button type="button" class="slick-arrow slick-prev"><img src="images/prev-arrow.svg" alt="prev slide"></button>',
         responsive: [
           {
             breakpoint: 1024,
