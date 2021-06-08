a7c0bb2ce619b6bc9567647c9038fd89777a1a9f
rwcalinin <rwcalinin@gmail.com>
2021-06-08 09:15:53 +0300

    slider

---
+++ src/js/script.js
@@ -0,0 +1,23 @@
+$(function() {
+    
+    $('.top-slider').slick({
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+
+            }
+          },
+          {
+            breakpoint: 640,
+            settings: {
+                arrows: false
+            }
+          }
+        ]
+      });
+
+});
